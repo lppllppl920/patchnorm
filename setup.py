46cb2a1722357cfacd4fe895b07d133c0e64f1b8
benjamindkilleen <benjamindkilleen@gmail.com>
2020-01-14 13:20:04 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='patchnorm',
+    version='0.0.1',
+    description='Implementation of the PatchNormConv2D and EfficientPatchNormConv2D layers.',
+    url='git@github.com:benjamindkilleen/patchnorm.git',
+    author='Benjamin D. Killeen',
+    author_email='killeen@jhu.edu',
+    license='unlicense',
+    packages=['patchnorm'],
+    zip_safe=False
+)
