cef246046cf2005b9ef6d323c864f2d1b09a999b
benjamindkilleen <benjamindkilleen@gmail.com>
2020-01-14 16:48:30 -0500

    got rid of weight loading

--- patchnorm/__init__.py
+++ patchnorm/__init__.py
@@ -1,4 +1,3 @@
 from .patch_norm_conv import PatchNormConv2D
 from .patch_norm_conv import EfficientPatchNormConv2D
-from .patch_norm_conv import load_traditional_weights
 
--- patchnorm/patch_norm_conv.py
+++ patchnorm/patch_norm_conv.py
@@ -221,37 +221,3 @@ class EfficientPatchNormConv2D(PatchNormConv2D):
       x = self.act(x)
 
     return x
-
-
-def load_traditional_weights(model, weights_path):
-  """Load the weights from a traditional model file into a model using PatchNorm.
-
-  Assumes that the conv layers in the weights file have a corresponding layer
-  in the model with the same name. Ignore BatchNorm layers if they are preceded
-  by a conv layer with a matching PatchNormConv2D or EfficientPatchNormConv2D
-  layer.
-
-  :param model: model to load the weights into
-  :param weights_path: path to the .h5 file containing the weights
-
-  """
-  source_model = keras.models.load_model(weights_path)
-  loaded_patchnorm_weights = False
-  
-  for i, source_layer in enumerate(source_model.layers):
-    if source_layer.count_params() == 0:
-      continue
-
-    if isinstance(source_layer, keras.layers.BatchNormalization) and loaded_patchnorm_weights:
-      continue
-
-    target_layer = model.get_layer(source_layer.name)
-    if issubclass(type(target_layer), PatchNormConv2D):
-      target_layer.set_conv_weights(source_layer.get_weights())
-      loaded_patchnorm_weights = True
-    else:
-      target_layer.set_weights(source_layer.get_weights())
-      loaded_patchnorm_weights = False
-
-  del source_model
-  return model
