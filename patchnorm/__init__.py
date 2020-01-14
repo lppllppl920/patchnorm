738cda452bd3fb8beb1badd66c94ed55b218f315
benjamindkilleen <benjamindkilleen@gmail.com>
2020-01-14 15:08:50 -0500

    fixed it

--- patchnorm/__init__.py
+++ patchnorm/__init__.py
@@ -1,4 +1,4 @@
 from .patch_norm_conv import PatchNormConv2D
 from .patch_norm_conv import EfficientPatchNormConv2D
-from .utils import load_traditional_weights
+from .patch_norm_conv import load_traditional_weights
 
